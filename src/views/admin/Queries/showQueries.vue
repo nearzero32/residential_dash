7f08b53053b8c0e19fb93391f41ec1b09b74f346
sjadabd <98034328+sjadabd@users.noreply.github.com>
2024-09-08 14:20:16 +0300

    Update showQueries.vue

--- src/views/admin/Queries/showQueries.vue
+++ src/views/admin/Queries/showQueries.vue
@@ -42,6 +42,7 @@
               hide-details
               single-line
               @click:clear="getCenter"
+              @input="getCenter"
               clearable
             />
           </VCol>
