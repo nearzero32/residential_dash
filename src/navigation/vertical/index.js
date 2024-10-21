1738f0df0104d4b79d31c936b4033247c3554ab1
sjadabd <98034328+sjadabd@users.noreply.github.com>
2024-10-21 15:11:00 +0300

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -376,6 +376,10 @@ export default [
     children: [
       { title: "Maintenance Staff", to: "admin-show-maintenance-staff" },
       {
+        title: "Maintenance of Devices",
+        to: "admin-show-maintenance-of-devices",
+      },
+      {
         title: "Service Bookings",
         type: "resident_manager",
         building_type: "منازل وشقق",
@@ -580,6 +584,10 @@ export default [
     icon: { icon: "mdi-cogs" },
     children: [
       { title: "Maintenance Staff", to: "admin-show-maintenance-staff" },
+      {
+        title: "Maintenance of Devices",
+        to: "admin-show-maintenance-of-devices",
+      },
       {
         title: "Service Bookings",
         type: "admin",
