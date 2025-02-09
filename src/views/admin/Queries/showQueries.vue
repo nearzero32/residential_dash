575d1d0e2608d418e730dc1282f3fd76403c2f8b
sjadabd <98034328+sjadabd@users.noreply.github.com>
2025-02-09 12:27:18 +0300

    Update showQueries.vue

--- src/views/admin/Queries/showQueries.vue
+++ src/views/admin/Queries/showQueries.vue
@@ -21,6 +21,16 @@
             >
               {{ t("Addition") }}
             </VBtn>
+            <VBtn
+              style="margin-right: 10px"
+              tile
+              color="success"
+              prepend-icon="fa-download"
+              :loading="xlsxData.listLoading"
+              @click="getAllDataAxios"
+            >
+              {{ t("Download Excel") }}
+            </VBtn>
           </VCol>
         </VRow>
       </VCardText>
@@ -28,10 +38,7 @@
 
     <VCard>
       <VCardTitle>
-        <VRow
-          justify="space-between"
-          style="align-items: center; margin-bottom: 15px"
-        >
+        <VRow justify="space-between" style="align-items: center; margin-bottom: 15px">
           <VCol cols="12" sm="12" md="12">
             <VTextField
               v-model="table.search"
@@ -108,11 +115,7 @@
           <VBtn color="primary" text @click="addDialog.open = false">
             {{ t("Cancel") }}
           </VBtn>
-          <VBtn
-            color="primary"
-            :loading="addDialog.saveLoading"
-            @click="addCenter"
-          >
+          <VBtn color="primary" :loading="addDialog.saveLoading" @click="addCenter">
             {{ t("Addition") }}
           </VBtn>
         </VCardActions>
@@ -167,11 +170,7 @@
           <VBtn color="primary" text @click="dialogEdit.open = false">
             {{ t("Cancel") }}
           </VBtn>
-          <VBtn
-            color="primary"
-            :loading="dialogEdit.loading"
-            @click="editItemConform"
-          >
+          <VBtn color="primary" :loading="dialogEdit.loading" @click="editItemConform">
             {{ t("Edit") }}
           </VBtn>
         </VCardActions>
@@ -226,6 +225,7 @@
 import adminApi from "@/api/adminApi";
 import { useI18n } from "vue-i18n";
 import Table from "@/components/table.vue";
+import * as XLSX from "xlsx";
 
 export default {
   components: {
@@ -295,6 +295,14 @@ export default {
       },
       // dialogEdit
 
+      // xlsx
+      xlsxData: {
+        allData: [],
+        listLoading: false,
+        filename: `الاستعلامات.xlsx`,
+      },
+      // xlsx
+
       // Delete
       dialogDelete: {
         open: false,
@@ -328,8 +336,7 @@ export default {
         phone: [
           (value) => {
             if (!value) return this.t("This field is required");
-            if (value.length !== 11)
-              return this.t("Phone number must be 11 digits");
+            if (value.length !== 11) return this.t("Phone number must be 11 digits");
             return true;
           },
         ],
@@ -461,6 +468,106 @@ export default {
     },
     // Get Data
 
+    // Download Excel
+    async getAllDataAxios() {
+      this.xlsxData.listLoading = true;
+      const key =
+        this.tableOptions.sortBy && this.tableOptions.sortBy.length > 0
+          ? this.tableOptions.sortBy[0]
+          : "createdAt";
+      const order =
+        this.tableOptions.sortDesc && this.tableOptions.sortDesc.length > 0
+          ? this.tableOptions.sortDesc[0]
+            ? "desc"
+            : "asc"
+          : "desc";
+
+      const sortByJSON = JSON.stringify({ key, order });
+
+      let { page, itemsPerPage } = this.tableOptions;
+
+      if (!page) {
+        page = 1;
+      }
+      if (!itemsPerPage) {
+        itemsPerPage = 10;
+      }
+
+      try {
+        const response = await adminApi.getQueries({
+          page,
+          limit: itemsPerPage,
+          sortBy: sortByJSON,
+          search: this.table.search,
+        });
+        this.xlsxData.allData = response.data.results.data;
+        this.handleDownload();
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          this.$store.dispatch("submitLogout");
+        } else if (error.response && error.response.status === 500) {
+          this.xlsxData.downloadLoading = false;
+          this.showDialogfunction(error.response.data.message, "#FF5252");
+        } else if (error.response && error.response.data.error === true) {
+          this.xlsxData.downloadLoading = false;
+          this.showDialogfunction(error.response.data.message, "#FF5252");
+        }
+      } finally {
+        this.xlsxData.listLoading = false;
+      }
+    },
+    async handleDownload() {
+      const header = [
+        this.t("Customer name"),
+        this.t("Phone number"),
+        this.t("Sales representative's name"),
+        this.t("Date"),
+      ];
+
+      const dataForExcel = [
+        header,
+        ...this.xlsxData.allData.map((item) => [
+          item.name,
+          item.phone,
+          item.employee_name,
+          item.createdAt,
+        ]),
+      ];
+
+      const ws = XLSX.utils.aoa_to_sheet(dataForExcel);
+
+      ws["!cols"] = [
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+        { wch: 30 },
+      ];
+
+      const headerStyle = {
+        font: { bold: true, color: { rgb: "#9155fd" } },
+        fill: { fgColor: { rgb: "0000FF" } },
+      };
+
+      header.forEach((col, index) => {
+        const cellAddress = XLSX.utils.encode_cell({ r: 0, c: index });
+        if (!ws[cellAddress]) ws[cellAddress] = { t: "s", v: col };
+        ws[cellAddress].s = headerStyle;
+      });
+
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+      this.xlsxData.listLoading = false;
+
+      XLSX.writeFile(wb, this.xlsxData.filename);
+    },
+    // Download Excel
+
     // Add Data
     async addCenter() {
       const { valid } = await this.$refs.form.validate();
@@ -548,9 +655,7 @@ export default {
     async deleteItemConfirm() {
       this.dialogDelete.loading = true;
       try {
-        const response = await adminApi.removeQueries(
-          this.dialogDelete.deletedItem._id
-        );
+        const response = await adminApi.removeQueries(this.dialogDelete.deletedItem._id);
         this.dialogDelete.loading = false;
         this.dialogDelete.open = false;
         this.getCenter();
